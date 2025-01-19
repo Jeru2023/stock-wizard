b9ae24b47315109014d56a5471521abf732bd7a7
Jeru2023 <jeru.token@gmail.com>
2025-01-19 09:49:55 +0800

    add daily stock prices table

--- database/schema.sql
+++ database/schema.sql
@@ -1,4 +1,5 @@
 CREATE DATABASE `stock-wizard`;
+
 CREATE TABLE `tickers` (
     `symbol` VARCHAR(20) PRIMARY KEY,  -- 股票代码，设为主键
     `name` VARCHAR(255) NOT NULL,      -- 股票名称
@@ -6,4 +7,17 @@ CREATE TABLE `tickers` (
     `exchange` VARCHAR(50),            -- 市场（如 NASDAQ、NYSE、上交所A股）
     `ipo_date` DATE,                   -- IPO Date
     `update_time` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP  -- 更新时间，自动更新
+);
+
+CREATE TABLE daily_stock_prices
+(
+    date      date           not null,
+    symbol    varchar(10)    not null,
+    adj_close decimal(10, 4) null,
+    close     decimal(10, 4) null,
+    high      decimal(10, 4) null,
+    low       decimal(10, 4) null,
+    open      decimal(10, 4) null,
+    volume    bigint         null,
+    primary key (date, symbol)
 );
